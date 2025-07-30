eb0b0568a34e2e1d1c035fa6f1758542ff4411c3
TheRealComputer <ibm7862@gmail.com>
2025-07-30 17:34:20 -0400

    Update configuration for refugee-uplift-website repository

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/refugee-uplift-website">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/team" element={<Team />} />
